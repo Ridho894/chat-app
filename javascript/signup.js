426ab0161075d141ca5b2e5b5f6617b26744eec3
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-09 08:25:31 +0700

    signup 100% ok dan bisa menyimpan image di folder

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,5 +1,6 @@
 const form = document.querySelector(".signup form"),
-    continueBtn = form.querySelector(".button input");
+    continueBtn = form.querySelector(".button input"),
+    errorText = form.querySelector(".error-txt");
 
 form.onsubmit = (e) => {
     e.preventDefault(); // Preventing form from submitting
@@ -7,15 +8,22 @@ form.onsubmit = (e) => {
 
 continueBtn.onclick = () => {
     // Ajax Start
-    let xhr = new XMLHttpRequest(); // Creating XML Object
+    let xhr = new XMLHttpRequest(); // Creating XML OBJECT
     xhr.open("POST", "../php/signup.php", true);
     xhr.onload = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 let data = xhr.response;
-                console.log(data);
+                if (data == "success") {
+                    location.href = "users.php";
+                } else {
+                    errorText.textContent = data;
+                    errorText.style.display = "block";
+                }
             }
         }
     }
-    xhr.send();
+    // Send form data through ajax to php
+    let formData = new FormData(form); // Creating new formData OBJECT
+    xhr.send(formData); // Sending the form data to php
 }
