39db20cb714c1bff246eb98f733ada2a09c1caaf
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-09 22:53:32 +0700

    menampilkan data kontak

--- javascript/users.js
+++ javascript/users.js
@@ -1,8 +1,23 @@
 const searchBar = document.querySelector(".users .search input"),
-searchBtn = document.querySelector(".users .search button");
+    searchBtn = document.querySelector(".users .search button"),
+    usersList = document.querySelector(".users .users-list")
 
 searchBtn.onclick = () => {
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
 }
+
+setInterval(() => {// Ajax Start
+    let xhr = new XMLHttpRequest(); // Creating XML OBJECT
+    xhr.open("GET", "../php/users.php", true);
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    xhr.send();
+}, 500) // Function will run frequently after 500ms
--- pages/users.php
+++ pages/users.php
@@ -18,8 +18,8 @@
                 }
             ?>
                 <div class="content">
-                    <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt="">
+                    <img src="../php/images/<?php echo $row['img'] ?>"
+                        alt=".img">
                     <div class="details">
                         <span><?php echo $row['fname'] . " " . $row["lname"]; ?></span>
                         <p><?php echo $row['status']; ?></p>
@@ -33,81 +33,7 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="a">
-                        <div class="details">
-                            <span>Yusuf</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot">
-                        <i class="fas fa-circle"></i>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="a">
-                        <div class="details">
-                            <span>Yusuf</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot">
-                        <i class="fas fa-circle"></i>
-                    </div>
-                </a><a href="#">
-                    <div class="content">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="a">
-                        <div class="details">
-                            <span>Yusuf</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot">
-                        <i class="fas fa-circle"></i>
-                    </div>
-                </a><a href="#">
-                    <div class="content">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="a">
-                        <div class="details">
-                            <span>Yusuf</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot">
-                        <i class="fas fa-circle"></i>
-                    </div>
-                </a><a href="#">
-                    <div class="content">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="a">
-                        <div class="details">
-                            <span>Yusuf</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot">
-                        <i class="fas fa-circle"></i>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt="a">
-                        <div class="details">
-                            <span>Yusuf</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot">
-                        <i class="fas fa-circle"></i>
-                    </div>
-                </a>
+                
             </div>
         </section>
     </div>
