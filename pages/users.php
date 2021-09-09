a37420d09fe8609fbc45333cb6c6896ff37d0877
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-09 22:32:55 +0700

    mencoba menggunakan unique_id, dan validasi OK

--- pages/users.php
+++ pages/users.php
@@ -1,25 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+    session_start();
+    if (!isset($_SESSION['unique_id'])) {
+        header("location: login.php");
+    }
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Realtime Chat App | PhpLearning</title>
-    <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
-
+<?php include_once '../header.php' ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+            <?php
+                include_once "../php/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                if (mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+                }
+            ?>
                 <div class="content">
                     <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt="">
                     <div class="details">
-                        <span>Chat App</span>
-                        <p>Active Now</p>
+                        <span><?php echo $row['fname'] . " " . $row["lname"]; ?></span>
+                        <p><?php echo $row['status']; ?></p>
                     </div>
                 </div>
                 <a href="index.php" class="logout">Logout</a>
